e6e31db28d3e8c2b5e4d8e46ba303fde8cc9a634
Connor Hamilton <clyhamilton@gmail.com>
2023-09-21 12:44:32 -0400

    Update 2023_09_20_195824_create_jobs_table.php

--- JobProject/database/migrations/2023_09_20_195824_create_jobs_table.php
+++ JobProject/database/migrations/2023_09_20_195824_create_jobs_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('address');
             $table->string('education');
             $table->integer('yearsofexperience');
-            $table->string('other'); 
+            $table->string('other');
         });
     }
 
